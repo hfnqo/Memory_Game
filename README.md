e61973be55dd2acdba710a343a6c50ad67b36faf
hfnqo <joushitsu22@gmail.com>
2023-01-05 22:00:25 +0800

    Memory Game

---
+++ README.md
@@ -0,0 +1,10 @@
+# Memory game
+A simple poker number memory matching game web app
+
+## Features
+- Tap a set of cards to match numbers
+
+### Game Method
+- 10 points for a successful match
+- When pairing fails, the card will be re-covered
+- The game ends when all cards are matched successfully
---
+++ main.js
@@ -0,0 +1,175 @@
+const GAME_STATE = {
+  FirstCardAwaits: 'FirstCardAwaits',
+  SecondCardAwaits: 'SecondCardAwaits',
+  CardsMatchFailed: 'CardsMatchFailed',
+  CardsMatched: 'CardsMatched',
+  GameFinished: 'GameFinished'
+}
+
+// Symbols = ['Spades', 'Hearts', 'Diamonds', 'Clubs']
+const Symbols = [
+  'https://assets-lighthouse.alphacamp.co/uploads/image/file/17989/__.png',
+  'https://assets-lighthouse.alphacamp.co/uploads/image/file/17992/heart.png',
+  'https://assets-lighthouse.alphacamp.co/uploads/image/file/17991/diamonds.png',
+  'https://assets-lighthouse.alphacamp.co/uploads/image/file/17988/__.png'
+]
+
+const view = {
+  getCardElement (index) {
+    return `<div class='card back' data-index="${index}"></div>`
+  },
+
+  getCardContent (index) {
+    const number = this.transformNumber((index % 13) + 1)
+    const symbol = Symbols[Math.floor(index / 13)]
+    return `
+      <p>${number}</p>
+      <img src='${symbol}'>
+      <p>${number}</p>
+    `
+  },
+
+  transformNumber (number) {
+    switch (number) {
+      case 1:
+        return 'A'
+      case 11:
+        return 'J'
+      case 12:
+        return 'Q'
+      case 13:
+        return 'K'
+      default:
+        return number
+    }
+  },
+
+  displayCards (indexes) {
+    const rootElement = document.querySelector('#cards')
+    rootElement.innerHTML = indexes.map(index => this.getCardElement(index)).join('')
+  },
+
+  flipCards (...cards) {
+   cards.map(card => {
+     if (card.matches('.back')) {
+       // 回傳正面
+       card.classList.remove('back')
+       card.innerHTML = this.getCardContent(Number(card.dataset.index))
+       return
+     }
+     // 回傳背面
+     card.classList.add('back')
+     card.innerHTML = null
+   })
+  },
+
+  pairCards (...cards) {
+    cards.map(card => {
+      card.classList.add('paired')
+    })
+  },
+
+  renderScore (score) {
+    document.querySelector('.score').textContent = `Score: ${score}`
+  },
+
+  renderTriedTimes (times) {
+    document.querySelector('.tried').textContent = `You've tried: ${times} times`
+  },
+
+  appendWrongAnimationend (...cards) {
+    cards.map(card => {
+      card.classList.add('wrong')
+      card.addEventListener('animationend', e => e.target.classList.remove('wrong'), {once: true})
+    })
+  },
+  showGameFinished () {
+    const header = document.querySelector('#header')
+    const div = document.createElement('div')
+    div.classList.add('completed')
+    div.innerHTML = `
+      <p>Complete!</p>
+      <p>Score: ${model.score}</p>
+      <p>You've tried: ${model.triedTimes} times</p>
+    `
+    header.before(div)
+  }
+}
+
+const model = {
+  revealedCards: [],
+  isRevealedCardsMatched() {
+    return this.revealedCards[0].dataset.index % 13 === this.revealedCards[1].dataset.index % 13
+  },
+  score: 0,
+  triedTimes: 0
+}
+
+const controller = {
+  currentState: GAME_STATE.FirstCardAwaits,
+  generateCards() {
+    view.displayCards(utility.getRandomNumberArray(52))
+  },
+
+  dispatchCardAction (card) {
+    if (!card.matches('.back')) return
+
+    switch (this.currentState) {
+      case GAME_STATE.FirstCardAwaits:
+        view.flipCards(card)
+        model.revealedCards.push(card)
+        this.currentState = GAME_STATE.SecondCardAwaits
+        break
+      case GAME_STATE.SecondCardAwaits:
+        view.renderTriedTimes(++model.triedTimes)
+        view.flipCards(card)
+        model.revealedCards.push(card)
+        // 判斷配對是否成功
+        if (model.isRevealedCardsMatched()) {
+          // 配對成功
+          this.currentState = GAME_STATE.CardsMatched
+          view.renderScore(model.score += 10)
+          view.pairCards(...model.revealedCards)
+          model.revealedCards = []
+          if (model.score === 260) {
+            this.currentState = GAME_STATE.GameFinished
+            view.showGameFinished()
+            return
+          }
+          this.currentState = GAME_STATE.FirstCardAwaits
+        } else {
+          // 配對失敗
+          this.currentState = GAME_STATE.CardsMatchFailed
+          view.appendWrongAnimationend(...model.revealedCards)
+          setTimeout(this.resetCards, 1000)
+        }
+        break
+    }
+  },
+
+  resetCards () {
+    view.flipCards(...model.revealedCards)
+    model.revealedCards = []
+    controller.currentState = GAME_STATE.FirstCardAwaits
+  }
+}
+
+const utility = {
+  getRandomNumberArray (count) {
+    const number = Array.from(Array(count).keys())
+    for (let index = number.length - 1; index > 0; index--) {
+      const randomIndex = Math.floor(Math.random() * (index + 1))
+      ;[number[index], number[randomIndex]] = [number[randomIndex], number[index]]
+    }
+    return number
+  }
+}
+
+controller.generateCards()
+
+document.querySelectorAll('.card').forEach(card => {
+  card.addEventListener('click', e => {
+    controller.dispatchCardAction(card)
+  })
+})
+
